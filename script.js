440bdd85b9f90783ed2e16518f167b7a5c70693b
golukumarpanit <121001986+golukumarpanit@users.noreply.github.com>
2025-03-10 13:42:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -33,4 +33,16 @@ document.addEventListener("DOMContentLoaded", function () {
         autoSlide();
     }
 });
+// yaha se slidr hai imag 
+document.addEventListener("DOMContentLoaded", function() {
+    let images = document.querySelectorAll(".kyashreegman");
+    let index = 0;
 
+    function changeImage() {
+        images[index].classList.remove("active");
+        index = (index + 1) % images.length;
+        images[index].classList.add("active");
+    }
+
+    setInterval(changeImage, 3000);
+});
