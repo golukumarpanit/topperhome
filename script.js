dcd417354349776b17a9e508b8a1aadf6065fd48
golukumarpanit <121001986+golukumarpanit@users.noreply.github.com>
2025-03-10 09:09:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("jobButton").onclick = function() {
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeDF-N7G-uq47AZakGkrWZ3D76xbMA-zENRhmKRupZhIXuHIw/viewform";
+    };
+});
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("naredramodi").onclick = function() {
+        window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeDF-N7G-uq47AZakGkrWZ3D76xbMA-zENRhmKRupZhIXuHIw/viewform";
+    };
+});
